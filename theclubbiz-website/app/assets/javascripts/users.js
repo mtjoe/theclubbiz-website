b28e17cf0ce1aa12dc445d0013ad686e99fa04a2
erlanggagama <erlangga.gama@gmail.com>
2014-05-12 14:22:30 +1000

    add register user

---
+++ theclubbiz-website/app/assets/javascripts/users.js
@@ -0,0 +1,22 @@
+/*$(window).load(function(){
+
+	$(".tf").focusin(function(){
+		$(this).css({padding: "10px", border:"4px solid gray"});
+		$(this).animate({width: "300px", height: "30px"}, 500);
+	});
+})*/
+
+
+function selectTextField(id) {
+	alert(id);
+	var this = $("#" + id));
+	//$(this).css({padding: "10px", border:"4px solid gray"});
+	//$(this).animate({width: "300px", height: "30px"}, 500);
+	
+}
+
+function leaveField() {
+	var $this = $(".tf");
+	//$this.animate({width:"300px", height:"25px"}, 500); 
+}
+
