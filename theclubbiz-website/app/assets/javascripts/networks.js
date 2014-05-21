ad3adf6b9afe62e4be785d1d0f4e14ee823be4c9
erwinfernandez92 <erwinfernandez92@gmail.com>
2014-05-21 16:11:04 +1000

    working on network

---
+++ theclubbiz-website/app/assets/javascripts/networks.js
@@ -0,0 +1,8 @@
+$(window).load(function() {
+	$('.societies textarea').tagsInput({
+    // my parameters here
+		"width": "75%",
+		"height": "70px",
+		"defaultText":'add  societies email here'
+	});
+});
