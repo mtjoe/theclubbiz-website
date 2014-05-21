b2884743bbf84ae5574888c82c48217e79516ec4
erwinfernandez92 <erwinfernandez92@gmail.com>
2014-05-21 13:29:18 +1000

    changed event database

--- theclubbiz-website/app/models/event.rb
+++ theclubbiz-website/app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ActiveRecord::Base
 
   # Many-to-many associations
-  has_many :EventFollower
-  has_many :SocietyEvent
+  has_many :Follower, :through => :EventFollower, :source => 'User'
+  has_many :Society, through :SocietyEvent
 
   # Many-to-one associations
   has_many :EventFeedback
